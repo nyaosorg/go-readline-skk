85c8e129ec74a389cb12b93b33f8dcc3c979f774
HAYAMA_Kaoru <iyahaya@nifty.com>
2023-07-15 19:38:42 +0900

    Implement loadWithConfigString

--- ondemand.go
+++ ondemand.go
@@ -27,19 +27,12 @@ func (o *onDemandLoad) String() string {
 	return "SKK_MODE_ONDEMAND_SETUP"
 }
 
-func (o *onDemandLoad) Call(ctx context.Context, B *readline.Buffer) readline.Result {
-	env, ok := o.Func()
-	if !ok {
-		readline.GlobalKeyMap.BindKey(o.Key, nil)
-		return readline.CONTINUE
-	}
-	skkMode := New()
-	ok = true
-	failed := false
-	succeeded := false
-	for ok {
+func loadWithConfigString(config string) (skkMode *Mode, closer func() error, errs []error) {
+	skkMode = New()
+	closer = func() error { return nil }
+	for ok := true; ok; {
 		var token string
-		token, env, ok = strings.Cut(env, ";")
+		token, config, ok = strings.Cut(config, ";")
 
 		key, value, hasEqual := strings.Cut(token, "=")
 		var err error
@@ -50,7 +43,7 @@ func (o *onDemandLoad) Call(ctx context.Context, B *readline.Buffer) readline.Re
 					err = nil
 				}
 				if err == nil {
-					o.closer = func() error {
+					closer = func() error {
 						return skkMode.SaveUserJisyo(value)
 					}
 				}
@@ -61,27 +54,37 @@ func (o *onDemandLoad) Call(ctx context.Context, B *readline.Buffer) readline.Re
 			err = skkMode.System.Load(token)
 		}
 		if err != nil {
-			fmt.Fprintf(B.Out, "\n%s", err.Error())
-			failed = true
-		} else {
-			succeeded = true
+			errs = append(errs, err)
 		}
 	}
-	if failed {
+	return skkMode, closer, errs
+}
+
+func (o *onDemandLoad) Call(ctx context.Context, B *readline.Buffer) readline.Result {
+	config, ok := o.Func()
+	if !ok {
+		readline.GlobalKeyMap.BindKey(o.Key, nil)
+		return readline.CONTINUE
+	}
+	skkMode, closer, errs := loadWithConfigString(config)
+	if len(errs) > 0 {
+		for _, e := range errs {
+			fmt.Fprintf(B.Out, "\n%s", e.Error())
+		}
 		B.RepaintAll()
+		return readline.CONTINUE
 	}
-	if succeeded {
-		readline.GlobalKeyMap.BindKey(o.Key, skkMode)
-		readline.GlobalKeyMap.BindKey(keys.Enter, &readline.GoCommand{
-			Name: "SKK_ACCEPT_LINE_WITH_LATIN_MODE",
-			Func: skkMode.cmdAcceptLineWithLatinMode,
-		})
-		readline.GlobalKeyMap.BindKey(keys.CtrlC, &readline.GoCommand{
-			Name: "SKK_INTRRUPT_WITH_LATIN_MODE",
-			Func: skkMode.cmdIntrruptWithLatinMode,
-		})
-		return skkMode.Call(ctx, B)
-	}
+	o.closer = closer
+	readline.GlobalKeyMap.BindKey(o.Key, skkMode)
+	readline.GlobalKeyMap.BindKey(keys.Enter, &readline.GoCommand{
+		Name: "SKK_ACCEPT_LINE_WITH_LATIN_MODE",
+		Func: skkMode.cmdAcceptLineWithLatinMode,
+	})
+	readline.GlobalKeyMap.BindKey(keys.CtrlC, &readline.GoCommand{
+		Name: "SKK_INTRRUPT_WITH_LATIN_MODE",
+		Func: skkMode.cmdIntrruptWithLatinMode,
+	})
+	skkMode.Call(ctx, B)
 	return readline.CONTINUE
 }
 
