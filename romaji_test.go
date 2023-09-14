f7700dcde4d065bdbe8056f2956545f72695612e
HAYAMA_Kaoru <iyahaya@nifty.com>
2023-09-15 01:37:43 +0900

    Fix: romaji_test failed

--- romaji_test.go
+++ romaji_test.go
@@ -8,7 +8,7 @@ import (
 func TestRomaji(t *testing.T) {
 	for _, table := range []map[string]string{hiragana.table, katakana.table} {
 		for key := range table {
-			lastByte := key[len(key)-1]
+			lastByte := key[0]
 			if strings.IndexByte(romajiTrigger, lastByte) < 0 {
 				t.Fatalf("%c is not contained in `%s`", lastByte, romajiTrigger)
 			}
