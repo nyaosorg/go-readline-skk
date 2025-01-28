e17350f8d27e28bd06f1298793970881e8217373
HAYAMA_Kaoru <iyahaya@nifty.com>
2025-01-29 01:18:04 +0900

    Support `(concat)` in dictionaries

--- jisyo.go
+++ jisyo.go
@@ -5,6 +5,7 @@ import (
 	"bytes"
 	"fmt"
 	"io"
+	"math/big"
 	"os"
 	"os/user"
 	"path/filepath"
@@ -13,6 +14,8 @@ import (
 	"time"
 
 	"golang.org/x/text/encoding/japanese"
+
+	"github.com/hymkor/sxencode-go/parser"
 )
 
 const (
@@ -125,6 +128,81 @@ func (j *Jisyo) load(filename string) (time.Time, error) {
 	return stamp, j.Read(fd)
 }
 
+type symbol struct {
+	value string
+}
+
+type cons struct {
+	car any
+	cdr any
+}
+
+var parser1 = &parser.Parser[any]{
+	Cons:   func(car, cdr any) any { return &cons{car: car, cdr: cdr} },
+	Int:    func(n int64) any { return n },
+	BigInt: func(n *big.Int) any { return n },
+	Float:  func(f float64) any { return f },
+	String: func(s string) any { return s },
+	Array: func(list []any, dim []int) any {
+		array := make([]any, len(list))
+		for i, v := range list {
+			array[i] = v
+		}
+		return array
+	},
+	Keyword: func(s string) any { return s },
+	Rune:    func(r rune) any { return r },
+	Symbol:  func(s string) any { return symbol{value: s} },
+	Null:    func() any { return nil },
+	True:    func() any { return true },
+}
+
+var rxEscSeq = regexp.MustCompile(`\\[0-9]+`)
+
+func parseSx(source string) string {
+	sxpr, err := parser1.Read(strings.NewReader(source))
+	if err != nil {
+		return source
+	}
+	list := []any{}
+	for {
+		c, ok := sxpr.(*cons)
+		if !ok {
+			break
+		}
+		list = append(list, c.car)
+		sxpr = c.cdr
+	}
+	if len(list) < 1 {
+		return source
+	}
+	sym, ok := list[0].(symbol)
+	if !ok {
+		return source
+	}
+	switch sym.value {
+	case "concat":
+		var buffer strings.Builder
+		for _, v := range list[1:] {
+			if s, ok := v.(string); ok {
+				buffer.WriteString(s)
+			}
+		}
+		s := buffer.String()
+		s = rxEscSeq.ReplaceAllStringFunc(s, func(ss string) string {
+			var oct rune = 0
+			var b strings.Builder
+			for _, c := range ss[1:] {
+				oct = (oct * 8) + (c - '0')
+			}
+			b.WriteRune(oct)
+			return b.String()
+		})
+		return s
+	}
+	return source
+}
+
 func (j *Jisyo) readOne(line string, okuri bool) bool {
 	if len(line) > 2 && line[0] == ';' && line[1] == ';' {
 		if strings.HasPrefix(line, ariHeader) {
@@ -145,6 +223,9 @@ func (j *Jisyo) readOne(line string, okuri bool) bool {
 	for {
 		one, rest, ok := strings.Cut(lists, "/")
 		if one != "" {
+			if len(one) > 2 && one[0] == '(' && one[len(one)-1] == ')' {
+				one = parseSx(one)
+			}
 			values = append(values, one)
 		}
 		if !ok {
