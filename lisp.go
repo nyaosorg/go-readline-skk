f026771dd4e5c254ff02e9a31c0e3dc756fc3d2e
HAYAMA Kaoru <3752189+hymkor@users.noreply.github.com>
2025-09-03 17:01:20 +0900

    Adapted for sxencode-go v0.3.0 (math/big import no longer needed)

--- lisp.go
+++ lisp.go
@@ -3,9 +3,9 @@ package skk
 import (
 	"errors"
 	"fmt"
-	"math/big"
 	"os"
 	"regexp"
+	"strconv"
 	"strings"
 	"time"
 
@@ -21,12 +21,62 @@ type cons struct {
 	cdr any
 }
 
+var rawStringToLispString = strings.NewReplacer(
+	`\"`, `"`,
+	`\\`, `\`,
+)
+
+var (
+	rxFloat1     = regexp.MustCompile(`^[\-\+]?[0-9]+\.[0-9]+([eE][\-\+]?\d+)?$`)
+	rxFloat2     = regexp.MustCompile(`^[\-\+]?[0-9]+[eE][-+]?\d+$`)
+	rxInteger    = regexp.MustCompile(`^[\-\+]?[0-9]+$`)
+	rxHexInteger = regexp.MustCompile(`^#[Xx][\+\-]?[0-9A-Fa-f]+$`)
+	rxOctInteger = regexp.MustCompile(`^#[Oo][\+\-]?[0-7]+$`)
+	rxBinInteger = regexp.MustCompile(`^#[Bb][\+\-]?[01]+$`)
+)
+
+func tryParseAsFloat(token string) (any, bool, error) {
+	if !rxFloat1.MatchString(token) && !rxFloat2.MatchString(token) {
+		return nil, false, nil
+	}
+	val, err := strconv.ParseFloat(token, 64)
+	if err != nil {
+		return nil, true, err
+	}
+	return val, true, nil
+}
+
+func tryParseAsInt(token string) (any, bool, error) {
+	var val int64
+	var err error
+	if rxInteger.MatchString(token) {
+		val, err = strconv.ParseInt(token, 10, 64)
+	} else if rxHexInteger.MatchString(token) {
+		val, err = strconv.ParseInt(token[2:], 16, 64)
+	} else if rxOctInteger.MatchString(token) {
+		val, err = strconv.ParseInt(token[2:], 8, 64)
+	} else if rxBinInteger.MatchString(token) {
+		val, err = strconv.ParseInt(token[2:], 2, 64)
+	} else {
+		return nil, false, nil
+	}
+	return val, true, err
+}
+
+func tryParseAsNumber(token string) (any, bool, error) {
+	if val, ok, err := tryParseAsInt(token); ok {
+		return val, true, err
+	}
+	if val, ok, err := tryParseAsFloat(token); ok {
+		return val, true, err
+	}
+	return nil, false, nil
+}
+
 var parser1 = &parser.Parser[any]{
 	Cons:   func(car, cdr any) any { return &cons{car: car, cdr: cdr} },
-	Int:    func(n int64) any { return n },
-	BigInt: func(n *big.Int) any { return n },
-	Float:  func(f float64) any { return f },
-	String: func(s string) any { return s },
+	Number: tryParseAsNumber,
+	String: func(s string) any { return rawStringToLispString.Replace(s) },
 	Array: func(list []any, dim []int) any {
 		array := make([]any, len(list))
 		for i, v := range list {
